a89317cdd7eebdfcc11a7bb256804985b4caacca
Ritam Das <ritam.das3110@outlook.com>
2026-03-03 18:08:24 +0000

    implement version and changedir subcmd

--- internal/cli/commands.go
+++ internal/cli/commands.go
@@ -1,9 +1,19 @@
 package cli
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+
+	"charm.land/lipgloss/v2"
+	"github.com/nxrmqlly/silo/internal/config"
+	"github.com/nxrmqlly/silo/internal/fs"
+)
 
 // overriden during build time via ldflags
-var version = "dev"
+var Version = "dev"
 
 const helpCmdStr = `usage: silo [command]
 
@@ -16,14 +26,66 @@ commands:
   version     print version
   help        show this message`
 
-func cmdPwd() {}
+func cmdPwd() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		fmt.Println("silo is not setup. run `silo` to get started.")
+		os.Exit(1)
+	}
+	fmt.Println(cfg.NotesDir)
+}
 
-func cmdVersion() {}
+func cmdVersion() {
+	fmt.Printf("silo version %s %s/%s\n", Version, runtime.GOOS, runtime.GOARCH)
+}
 
-func cmdChangeDir() {}
+func cmdChangeDir() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		fmt.Println("silo is not set up. run `silo` to get started.")
+		os.Exit(1)
+	}
+
+	grey := lipgloss.NewStyle().Foreground(lipgloss.Color("243")).Italic(true)
+	purp := lipgloss.NewStyle().Foreground(lipgloss.Color("141"))
+	green := lipgloss.NewStyle().Foreground(lipgloss.Color("42"))
+
+	var prompt string = purp.Render("? new notes directory? ") +
+		grey.Render("("+cfg.NotesDir+")") + "\n> "
+
+	fmt.Print(prompt)
+	input := readLine()
+
+	if input == "" {
+		fmt.Println("no change.")
+		return
+	}
+
+	input = fs.ExpandHome(input)
+
+	if err := fs.CreateDir(input); err != nil {
+		fmt.Printf("error creating directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	cfg.NotesDir = input
+	if err := config.SaveConfig(cfg); err != nil {
+		fmt.Printf("error saving config: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(green.Render("notes directory changed to", input))
+
+}
 
 func cmdReset() {}
 
 func cmdHelp() {
 	fmt.Println(helpCmdStr)
+}
+
+func readLine() string {
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	return strings.TrimSpace(input)
 }
